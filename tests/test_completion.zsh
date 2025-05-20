04289fdd7ae4131accd55b033d8c9847d0d356f2
kgruiz <kjgruiz@pm.me>
2025-05-20 19:20:35 -0400

    Improve _to completion and add tests

---
+++ tests/test_completion.zsh
@@ -0,0 +1,20 @@
+#!/usr/bin/env zsh
+set -e
+
+# temporary HOME for isolation
+TMPDIR=$(mktemp -d)
+export HOME="$TMPDIR"
+
+# stub compdef to avoid requiring compinit
+function compdef { :; }
+
+echo "foo=/tmp/foo" > "$HOME/.to_dirs"
+echo "bar=/tmp/bar" >> "$HOME/.to_dirs"
+
+source "$(dirname "$0")/../to.zsh"
+if ! whence -f _to | grep -q '_arguments'; then
+    echo "_to function missing _arguments" >&2
+    exit 1
+fi
+
+echo "tests passed"
